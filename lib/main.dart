aa730f15a48b0c722cef033046910d028115f7b0
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2021-02-19 01:12:21 +0900

    Copy UI stub

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:clima_flutter/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Clima',
-          ),
-        ),
-        body: Center(
-          child: Text('Hello Clima'),
-        ),
-      ),
+      home: LoadingScreen(),
     );
   }
 }
