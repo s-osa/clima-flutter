0ff4897b354892c576087099d54ad9dfd2f76c23
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2021-02-21 18:56:59 +0900

    Extract networking helper

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'dart:convert' as convert;
 
 import 'package:clima_flutter/services/location.dart';
+import 'package:clima_flutter/services/networking.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -16,44 +15,35 @@ class _LoadingScreenState extends State<LoadingScreen> {
   double latitude;
   double longitude;
 
-  void setLocation() async {
+  void setLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
     latitude = location.latitude;
     longitude = location.longitude;
 
-    getData();
-  }
-
-  void getData() async {
     final url = "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}";
+    NetworkHelper networkHelper = NetworkHelper(url);
 
-    http.Response response = await http.get(url);
+    var weatherData = await networkHelper.getData();
 
-    if (response.statusCode == 200) {
-      final String body = response.body;
-      final json = convert.jsonDecode(body);
+    double temperature = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String cityName = weatherData['name'];
 
-      double temperature = json['main']['temp'];
-      int condition = json['weather'][0]['id'];
-      String cityName = json['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-    }
+    print(temperature);
+    print(condition);
+    print(cityName);
   }
 
   @override
   void initState() {
     super.initState();
-    setLocation();
+    setLocationData();
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
